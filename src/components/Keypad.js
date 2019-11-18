e8a6c47642cc6c6512a847b88037e027c48361aa
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-11-18 16:40:35 -0500

    met all specs

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+import React from "react";
+
+const onKeyUpHandler = () => console.log("Entering password...");
+
+const Keypad = () => {
+  return <input type="password" onKeyUp={onKeyUpHandler} />;
+};
+
+export default Keypad;
