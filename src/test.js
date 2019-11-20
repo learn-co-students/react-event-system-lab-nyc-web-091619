495f2beb564b7b279f9c9355097302fbbfb2caba
AdamBrownBB <adam.brown.betty@gmail.com>
2019-11-20 15:58:07 -0500

    Done.

---
+++ src/test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const test = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+test.propTypes = {
+    
+};
+
+
+export default test;
